1efb62d835ad6268f097e9ffacfb863feb687555
FrancisDoh <frankyb026@gmail.com>
2023-03-01 14:21:38 -0500

    3-1-2023

--- MiMik_Auto_Ins_RestfulApi/MiMik_Auto_Ins_RestfulApi/Program.cs
+++ MiMik_Auto_Ins_RestfulApi/MiMik_Auto_Ins_RestfulApi/Program.cs
@@ -1,6 +1,6 @@
 using MiMik_Auto_Ins_RestfulApi.Profiles;
-using System.Data;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // START
 builder.Services.AddScoped<IDbConnection>(db => new MySqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"))); // Connection to the MySQL DB
